44e34c0eb35309b4d2507e116f69a2ed2586ae76
AlexRDiaz <bugi2532@hotmail.com>
2023-07-17 15:38:49 -0500

    addeed advanced filter

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e0d75ea5b04801c273a3e3507f5cb2b4
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "63aae643040eceeac3b0202f10d630f6",
-"/": "63aae643040eceeac3b0202f10d630f6",
-"main.dart.js": "7fd319f976f991fdf52702f1fd4f15a9",
+"index.html": "8c48ca4bf7a88e39f29e517c1726a249",
+"/": "8c48ca4bf7a88e39f29e517c1726a249",
+"main.dart.js": "5d6ab5529538886e794350b2f53424e5",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can

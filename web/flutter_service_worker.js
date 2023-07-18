ec9fc99ddec378abdfca74a828f7416111dc6eca
AlexRDiaz <bugi2532@hotmail.com>
2023-07-18 08:25:38 -0500

    fixed responsive

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e0d75ea5b04801c273a3e3507f5cb2b4
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b1dab7f889deb9be6e607e4bf793c880",
-"/": "b1dab7f889deb9be6e607e4bf793c880",
-"main.dart.js": "4f23ee3b57b65eb827000bd00cf53a3e",
+"index.html": "a1f73fd274afc5a8755c0d4c277ac730",
+"/": "a1f73fd274afc5a8755c0d4c277ac730",
+"main.dart.js": "d841a99ce2d6a0b09a1b8daf96fabd65",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can
